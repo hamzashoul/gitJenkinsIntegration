b7942094e9cc289a60cd617a9f8c029e8c781309
HAMZA SHOUL <HSHOUL@groupebcp.com>
2024-08-23 17:47:23 +0100

    edit script.py

--- script.py
+++ script.py
@@ -24,16 +24,23 @@ def generate_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter):
 # Parameters for the Mandelbrot set
 xmin, xmax = -2.0, 1.0
 ymin, ymax = -1.5, 1.5
-width, height = 800, 800
-max_iter = 256
+width, height = 1000, 1000
+max_iter = 512
+cmap = 'inferno'  # Choose different color maps: 'viridis', 'plasma', 'inferno', 'magma', 'cividis'
 
 # Generate the Mandelbrot set
 mandelbrot_image = generate_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter)
 
-# Plotting the Mandelbrot set
-plt.imshow(mandelbrot_image.T, extent=[xmin, xmax, ymin, ymax], cmap='inferno', interpolation='bilinear')
+# Plotting the Mandelbrot set with the selected color map
+plt.imshow(mandelbrot_image.T, extent=[xmin, xmax, ymin, ymax], cmap=cmap, interpolation='bilinear')
 plt.colorbar()
-plt.title("Mandelbrot Set")
+plt.title("Mandelbrot Set (Zoom: [{}, {}] x [{}, {}])".format(xmin, xmax, ymin, ymax))
 plt.xlabel("Re")
 plt.ylabel("Im")
+
+# Save the image to a file
+output_filename = 'mandelbrot_set.png'
+plt.savefig(output_filename, dpi=300)
+print(f"Mandelbrot set image saved as {output_filename}")
+
 plt.show()
